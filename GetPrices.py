7c0118a60aa150972f31f44a49c5b48fcc4b9d50
= <omarabbasi16@gmail.com>
2024-12-08 16:05:15 -0500

    updated script to build a CSV with game prices

--- GetPrices.py
+++ GetPrices.py
@@ -1,26 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
 
-gamesList = []
 
 def getPrice(title):
+    price = "No Price Found"
     formattedTitle = title.lower().replace(":", "")
     titleForURL = formattedTitle.replace("'", "%27").replace(" ", "+")
     titleForDiv = formattedTitle.replace("'", "-").replace(" ", "-")
     URL = 'https://gg.deals/games/?title={}'.format(titleForURL)
     
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    game = soup.find('div', {"data-game-name": titleForDiv})
-    price = game.find('span', {"class": "price-inner numeric"}).get_text()
+    try:
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        game = soup.find('div', {"data-game-name": titleForDiv})
+        price = game.find('span', {"class": "price-inner numeric"}).get_text()
+    except AttributeError:
+        print('Could not retrieve pricing information for {}'.format(game))
     
     return price
 
-for game in gamesList:
-    try:
-        print("{}: {}".format(game, getPrice(game)))
+def buildCsv(titles):
+    data = [
+            ['Title', 'Price']
+        ]
+    for title in titles:
+        price = getPrice(title)
+        data.append([title, price])
+    
+    
+    try: 
+        csv_file_path = 'games.csv'
+        with open (csv_file_path, mode='w') as file:
+            for row in data:
+                file.write(','.join(map(str, row)) + '\n')
     except AttributeError:
-        print('Could not retrieve pricing information for {}'.format(game))
+        print('Error creating CSV')
 
 
 
+gamesList = []
+buildCsv(gamesList)
