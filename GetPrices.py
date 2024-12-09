c45a603c6ba10d6aaf7ac9a98673ca634defdc12
= <omarabbasi16@gmail.com>
2024-12-09 10:15:04 -0500

    refactor

--- GetPrices.py
+++ GetPrices.py
@@ -3,30 +3,44 @@ import requests
 import concurrent.futures
 
 
+def build_title_for_div(title):
+    return title.replace("'", "")\
+                .replace("!", "") \
+                .replace(" & ", " ") \
+                .replace(" ", "-") \
+                .replace("#", "") \
+                .replace(".", "") \
+                .replace("+", "") \
+                .replace("?", "")
+
+
+def get_gg_deals_soup(url):
+    page = requests.get(url)
+    return BeautifulSoup(page.content, "html.parser")
+    
+
+def find_game_from_soup(soup, title):
+    return soup.find('div', {"data-game-name": title})
+
+def get_price_from_game(game):
+    return game.find('span', {"class": "price-inner numeric"}).get_text().replace("~", "")
+
 def get_price(title):
     price = "No Price Found"
     formattedTitle = title.lower().replace(":", "")
     titleForURL = formattedTitle.replace("'", "%27").replace(" ", "+")
-    titleForDiv = formattedTitle.replace("'", "") \
-                                .replace("!", "") \
-                                .replace(" & ", " ") \
-                                .replace(" ", "-") \
-                                .replace("#", "") \
-                                .replace(".", "") \
-                                .replace("+", "") \
-                                .replace("?", "")
+    titleForDiv = build_title_for_div(formattedTitle)
     
     URL = 'https://gg.deals/games/?title={}'.format(titleForURL)
     
     try:
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, "html.parser")
-        game = soup.find('div', {"data-game-name": titleForDiv})
-        price = game.find('span', {"class": "price-inner numeric"}).get_text()
+        soup = get_gg_deals_soup(URL)
+        game = find_game_from_soup(soup, titleForDiv)
+        price = get_price_from_game(game)
     except AttributeError:
         print('Could not retrieve pricing information for {}'.format(title))
     
-    return title, price.replace("~", "")
+    return title, price
 
 def buildCsv(titles):
     data = [
